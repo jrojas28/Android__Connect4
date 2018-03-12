7ad78b6470505b254dc840ace641dfc4245f2064
Jaime Rojas <j_rojas94@hotmail.com>
2018-03-11 22:33:22 -0400

    Fixed bug which allowed game to continue if the alert dialog was skipped. Added back to menu button. Added animation for player icons.

--- app/src/main/java/com/altice_crt_b/connect4/activities/GameActivity.java
+++ app/src/main/java/com/altice_crt_b/connect4/activities/GameActivity.java
@@ -1,6 +1,9 @@
 package com.altice_crt_b.connect4.activities;
 
+import android.animation.ObjectAnimator;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +11,10 @@ import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.BounceInterpolator;
-import android.view.animation.GridLayoutAnimationController;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.altice_crt_b.connect4.R;
 import com.altice_crt_b.connect4.adapters.BoardGridAdapter;
@@ -26,6 +28,8 @@ public class GameActivity extends AppCompatActivity {
 
     private GridView chipsView;
     private AlertDialog gameEndDialog;
+    private Player player1;
+    private Player player2;
     private GameInstance gameInstance;
     private ArrayList<Integer> usedPositions;
 
@@ -36,12 +40,23 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.game_activity);
         Bundle extras = getIntent().getExtras();
         usedPositions = new ArrayList<>();
-        gameInstance = extras != null ?
-                //If extras isn't null, do this.
-                new GameInstance(new Player(extras.getString("player_1_username")), new Player(extras.getString("player_2_username")), extras.getInt("starting_player")) :
-                //If extras is null, do this.
-                new GameInstance(new Player(getString(R.string.default_p_1_name)), new Player(getString(R.string.default_p_2_name)));
+        //Initialize the players & the instance.
+        if(extras != null) {
+            player1 = new Player(extras.getString("player_1_username"));
+            player2 = new Player(extras.getString("player_2_username"));
+            gameInstance = new GameInstance(player1, player2, extras.getInt("starting_player"));
+        }
+        else {
+            player1 = new Player(getString(R.string.default_p_1_name));
+            player2 = new Player(getString(R.string.default_p_2_name));
+            gameInstance = new GameInstance(player1, player2);
+        }
+        //Set the names on the players to display on their respective textViews.
+        TextView p1Name = (TextView) findViewById(R.id.p1_name);
+        TextView p2Name = (TextView) findViewById(R.id.p2_name);
 
+        p1Name.setText(player1.getUsername());
+        p2Name.setText(player2.getUsername());
         gameEndDialog = new AlertDialog.Builder(GameActivity.this )
                 .setTitle(R.string.game_finished_title)
                 .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
@@ -49,9 +64,13 @@ public class GameActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialogInterface, int i) {
                     gameInstance.reset();
                     cleanBoard();
-//                            chipsView.setAdapter(new BoardGridAdapter(GameActivity.this, R.layout.chip_layout));
-
                 }
+                })
+                .setNegativeButton("Back to Main Menu", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
                 })
                 .create();
 
@@ -72,22 +91,27 @@ public class GameActivity extends AppCompatActivity {
     }
 
     public void handlePlay(int clickedItem){
+        //If the game isn't finished, handle the last play.
+        if( !gameInstance.getGameStatus() ) {
+            int column = clickedItem % 7;
+            int row = gameInstance.placeChip(column);
 
-        int column = clickedItem % 7;
-        int row = gameInstance.placeChip(column);
+            if(row != -1){
+                int position = row * 7  + column ;
+                placeChipAnimated(position);
+                usedPositions.add(position);
 
-        if(row != -1){
-            int position = row * 7  + column ;
-            placeChipAnimated(position);
-            usedPositions.add(position);
+                if( gameInstance.getGameStatus() ){
+                    gameEndDialog.setMessage( String.format( getString(R.string.game_end_content), gameInstance.getWinner().getUsername() ));
+                    gameEndDialog.show();
+                }
 
-            if( gameInstance.getGameStatus() ){
-                gameEndDialog.setMessage( String.format( getString(R.string.game_end_content), gameInstance.getWinner().getUsername() ));
-                gameEndDialog.show();
+                toggleTurn();
+
             }
-
-            gameInstance.toggleTurn();
-
+        }
+        else {
+            gameEndDialog.show();
         }
     }
 
@@ -117,8 +141,8 @@ public class GameActivity extends AppCompatActivity {
     public void removeChipAnimated(int position){
         View chipLayout = ( View )chipsView.getItemAtPosition(position);
         final ImageView chipView = chipLayout.findViewById(R.id.chip);
-        Animation bounceOnExit = AnimationUtils.loadAnimation(GameActivity.this, R.anim.bounce_on_exit);
-        bounceOnExit.setAnimationListener(new Animation.AnimationListener() {
+        Animation dropOnExit = AnimationUtils.loadAnimation(GameActivity.this, R.anim.drop_on_exit);
+        dropOnExit.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
 
@@ -134,6 +158,44 @@ public class GameActivity extends AppCompatActivity {
 
             }
         });
-        chipLayout.startAnimation(bounceOnExit);
+        chipLayout.startAnimation(dropOnExit);
+    }
+
+    public void toggleTurn() {
+        //Initialize Variables
+        Player activePlayer = gameInstance.toggleTurn();
+        int turn = gameInstance.getTurn();
+
+        //Find the views related to each player.
+        TextView p1Name = (TextView) findViewById(R.id.p1_name);
+        TextView p2Name = (TextView) findViewById(R.id.p2_name);
+        ImageView p1Pic = (ImageView) findViewById(R.id.p1_icon);
+        ImageView p2Pic = (ImageView) findViewById(R.id.p2_icon);
+        Animation bounce = AnimationUtils.loadAnimation(GameActivity.this, R.anim.bounce_loop);
+
+        //It's the 1st player's Turn.
+        if(turn == 1) {
+            ObjectAnimator fadeToWhite = ObjectAnimator.ofObject(p1Name, "textColor", new android.animation.ArgbEvaluator(), Color.argb(255,0,0,0), Color.argb(255,255,255,255));
+            fadeToWhite.setDuration(500);
+            fadeToWhite.start();
+
+            ObjectAnimator fadeToBlack = ObjectAnimator.ofObject(p2Name, "textColor", new android.animation.ArgbEvaluator(), Color.argb(255,255,255,255), Color.argb(255,0,0,0));
+            fadeToBlack.setDuration(500);
+            fadeToBlack.start();
+            p2Pic.clearAnimation();
+            p1Pic.startAnimation(bounce);
+        }
+        //It's the 2nd player's Turn.
+        else {
+            ObjectAnimator fadeToWhite = ObjectAnimator.ofObject(p2Name, "textColor", new android.animation.ArgbEvaluator(), Color.argb(255,0,0,0), Color.argb(255,255,255,255));
+            fadeToWhite.setDuration(500);
+            fadeToWhite.start();
+
+            ObjectAnimator fadeToBlack = ObjectAnimator.ofObject(p1Name, "textColor", new android.animation.ArgbEvaluator(), Color.argb(255,255,255,255), Color.argb(255,0,0,0));
+            fadeToBlack.setDuration(400);
+            fadeToBlack.start();
+            p1Pic.clearAnimation();
+            p2Pic.startAnimation(bounce);
+        }
     }
 }
